cef02ffe706040adbecf64e60ccf52cb0e404faa
SudoQ <simj91@gmail.com>
2015-05-15 13:56:59 +0200

    A rough shader of how it will work later

---
+++ share/SARndbox-1.5/Shaders/SurfaceAddVegetationColor.fs
@@ -0,0 +1,8 @@
+#extension GL_ARB_texture_rectangle : enable
+
+uniform sampler2DRect vegetationSampler;
+
+void addVegetationColor(in vec2 fragCoord, inout vec4 baseColor)
+	{
+	// Get value from sampler and change baseColor.
+	}
