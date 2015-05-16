66261e425b99a8a18469ab0f669a327afde56bd4
SudoQ <simj91@gmail.com>
2015-05-16 17:55:35 +0200

    Change base color according to the vegetation texture

--- share/SARndbox-1.5/Shaders/SurfaceAddVegetationColor.fs
+++ share/SARndbox-1.5/Shaders/SurfaceAddVegetationColor.fs
@@ -1,3 +1,7 @@
+/***********************************************************************
+SurfaceAddWaterColor - Shader fragment to modify the base color depening
+of the vegetation texture.
+***********************************************************************/
 #extension GL_ARB_texture_rectangle : enable
 
 uniform sampler2DRect vegetationSampler;
@@ -5,4 +9,6 @@ uniform sampler2DRect vegetationSampler;
 void addVegetationColor(in vec2 fragCoord, inout vec4 baseColor)
 	{
 	// Get value from sampler and change baseColor.
+	float value = texture2DRect(vegetationSampler, fragCoord.xy).r;
+	baseColor = vec4(0.0, float, 0.0, 0.0);
 	}
