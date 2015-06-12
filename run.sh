ea4538d3b72d30fb017dba2354aee8fdb91a02d7
SudoQ <simj91@gmail.com>
2015-06-12 17:24:19 +0200

    Added local add water to the config

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 PROJDIR="/home/sandbox/src/SARndbox"
 VRUICFG="$PROJDIR/etc/SARndbox-1.5/Vrui.cfg"
-$PROJDIR/bin/SARndbox -nhm -loadInputGraph $PROJDIR/SavedInputGraph0001.inputgraph -mergeConfig $VRUICFG
+$PROJDIR/bin/SARndbox -nhm -loadInputGraph $PROJDIR/SavedInputGraph0002.inputgraph -mergeConfig $VRUICFG
