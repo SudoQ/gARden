df5493582c675685b358226bd17bd06351d58b0c
SudoQ <simj91@gmail.com>
2015-06-17 17:50:41 +0200

    Double ; by mistake

--- share/SARndbox-1.5/Shaders/PreviousHydrationUpdateShader.fs
+++ share/SARndbox-1.5/Shaders/PreviousHydrationUpdateShader.fs
@@ -23,7 +23,7 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
 
 #extension GL_ARB_texture_rectangle : enable
 
-uniform sampler2DRect hydrationSampler;;
+uniform sampler2DRect hydrationSampler;
 
 void main()
 	{
