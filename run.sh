b6d0e74839d9feef938537a42ff668267a2f66d1
SudoQ <simj91@gmail.com>
2015-06-22 14:54:08 +0200

    Added 0.01 cm evaporation rate

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 PROJDIR="/home/sandbox/src/SARndbox"
 VRUICFG="$PROJDIR/etc/SARndbox-1.5/Vrui.cfg"
-$PROJDIR/bin/SARndbox -loadInputGraph $PROJDIR/SavedInputGraph0002.inputgraph -mergeConfig $VRUICFG
+$PROJDIR/bin/SARndbox -loadInputGraph $PROJDIR/SavedInputGraph0002.inputgraph -mergeConfig $VRUICFG -evr -0.01
