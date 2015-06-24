af49092ce69179be9a1ac489ae1f3f6c7837e89f
SudoQ <simj91@gmail.com>
2015-06-24 12:09:15 +0200

    Uses the old Vrui config now without local water tool

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 PROJDIR="/home/sandbox/src/SARndbox"
 VRUICFG="$PROJDIR/etc/SARndbox-1.5/Vrui.cfg"
-$PROJDIR/bin/SARndbox -loadInputGraph $PROJDIR/SavedInputGraph0002.inputgraph -mergeConfig $VRUICFG -evr -0.01
+$PROJDIR/bin/SARndbox -loadInputGraph $PROJDIR/SavedInputGraph0001.inputgraph -mergeConfig $VRUICFG -rs 0.0 -evr -0.01
