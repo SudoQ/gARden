29a44ea2b15d6967825296414c8543a76a806acf
SudoQ <simj91@gmail.com>
2015-06-25 10:30:47 +0200

    Enabled contour lines again

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 PROJDIR="/home/sandbox/src/SARndbox"
 VRUICFG="$PROJDIR/etc/SARndbox-1.5/Vrui.cfg"
-SBOPT="-rs 0.0 -evr -0.01 -ncl -fpv"
+SBOPT="-rs 0.0 -evr -0.01 -fpv"
 $PROJDIR/bin/SARndbox -loadInputGraph $PROJDIR/SavedInputGraph0001.inputgraph -mergeConfig $VRUICFG $SBOPT
